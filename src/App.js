720a3ae970d9b5a0b7a02c5108ba2e179d8ba75e
Hamza Koc <hamzakooc@gmail.com>
2021-11-21 19:17:44 -0500

    Axios example with some fetched user details

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import PersonList from './PersonList ';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Axios Lab Example </h1>
+      <h2>Hamza Koc 101274109 </h2>
+      <PersonList />
     </div>
   );
 }
